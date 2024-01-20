aaa57240c7a2884fe0aab1f63b38492d7f66cf84
marcornc <130748589+marcornc@users.noreply.github.com>
2024-01-20 12:19:16 +0000

    removed all the video / audio. changed the supabase

--- scripts/supabase.js
+++ scripts/supabase.js
@@ -1,11 +1,8 @@
+
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-// FinalProject 2 Database
-// const SUPABASE_URL = "https://fwsgzqviemhtvtdjwoas.supabase.co";
-// const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ3c2d6cXZpZW1odHZ0ZGp3b2FzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE3ODEyNzgsImV4cCI6MjAxNzM1NzI3OH0.IbnD8zXW2H5VzrKna_8VUWkQEEahSTm9-YMkHfmb2tA";
-// export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+const supabaseUrl = "https://rwharymszlgtaxvpvotb.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ3aGFyeW1zemxndGF4dnB2b3RiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU3NDQ4MjAsImV4cCI6MjAyMTMyMDgyMH0.g1pQub4PeODpSFsD2WNZxCtQ2we323WeBGEQTwoM27I";
 
-// FinalProject database
-const SUPABASE_URL = "https://nzrxvqmohrkorfxfclhh.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im56cnh2cW1vaHJrb3JmeGZjbGhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDExOTQ3MjcsImV4cCI6MjAxNjc3MDcyN30.DxyR6gGrCM7VxJGl7ILum76LVkzgoSnk6gUwxug8Fy0";
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
